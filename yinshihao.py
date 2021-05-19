4f3cee8e3fbcc044f3c83ebf3697c80e239e1495
chisiliangmi <2993436102@qq.com>
2021-05-19 12:12:32 +0800

    second commmit yinshihao.py

--- yinshihao.py
+++ yinshihao.py
@@ -3,8 +3,9 @@ from cal_num import CalNum
 
 if __name__ == "__main__":
     file_path = '中欧班列（满、二）运行情况表2021051516.xlsx'
-    city1 = '满洲里'
-    city2 = '二连'
     cal_num = CalNum(file_path)
-    cal_num.run_cal_num(city1, city2)
-    cal_num.run_cal_num_(city1, city2)
+    citys = ['满洲里','二连','绥芬河']
+    for i in range(len(citys)):
+        city = citys[i]
+        cal_num.run_cal_num(city)
+        cal_num.run_cal_num_(city)
